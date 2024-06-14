e28d1cd4573d752ecee824109219f1ece27f6beb
VEZDO123 <163718030+VEZDO123@users.noreply.github.com>
2024-06-14 20:32:49 +0400

    Add files via upload

---
+++ sabavezdeni/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
